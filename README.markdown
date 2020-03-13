249ad71242e677d4e1669b938905ed0252256147
Paul Coster <58193305+PLCoster@users.noreply.github.com>
2020-03-12 22:17:16 -0700

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# FreeCodeCamp: Front End Libs - Random Quote Generator
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/plcoster/pen/PoqjQpR](https://codepen.io/plcoster/pen/PoqjQpR).
+
+ 
---
+++ src/script.js
@@ -0,0 +1,43 @@
+const endpoint = 'https://thesimpsonsquoteapi.glitch.me/quotes';
+
+function getQuote() {
+  fetch(endpoint).
+  then(function (response) {
+    return response.json();
+  }).
+  then(function (data) {
+    //console.log(data);
+    displayQuote(data);
+  }).
+  catch(function () {
+    console.log("An error occurred - API offline?");
+    let data = { quote: 'Do\'h! - The Simpson Quote API is offline!', character: 'Homer' };
+    console.log(data);
+    displayQuote(data);
+  });
+}
+
+function displayQuote(data) {
+  const quoteText = document.querySelector('.quote-text');
+  const authText = document.querySelector('.author');
+  const body = document.querySelector('.body');
+
+  let quotedcont = data[0].quote;
+
+  quoteText.textContent = quotedcont;
+  authText.textContent = "- " + data[0].character;
+  body.setAttribute('style', `background-image: url(${data[0].image}`);
+
+  const tweetButton = document.querySelector('.tweet');
+  //Truncate tweet to max 280 chars:
+  if (quotedcont.length + 3 + data[0].character.length > 280) {
+    quotedcont = quotedcont.slice(0, 280 - 6 - data[0].character.length) + '...';
+  }
+  tweetButton.setAttribute('href', `https://twitter.com/intent/tweet?related=freecodecamp&text=${quotedcont} - ${data[0].character}`);
+}
+
+const newQuoteButton = document.querySelector('.new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+
+window.onload = getQuote();
